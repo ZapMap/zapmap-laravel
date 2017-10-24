7b8a853c6092126648cea9d9310de1920f73bd73
Jeff Kilroy <jeffkilroy@gmail.com>
2017-10-24 15:24:37 -0400

    Update ZapMap.php
    implement ZapMapInterface

--- src/ZapMap.php
+++ src/ZapMap.php
@@ -5,7 +5,7 @@ namespace ZapMap\ZapMapLaravel;
 use ZapMap\ZapMapLaravel\API\Exceptions\ClientException;
 use ZapMap\ZapMapLaravel\API\Exceptions\InvalidKeyException;
 
-class ZapMap{
+class ZapMap implements ZapMapInterface {
 
     public function call($method, $uri, $data=[]){
         $client = new \ZapMap\ZapMapLaravel\API\Client();
