f49fd1575ff7f7d57ec226db4ab3fe628c17503f
Jeff Kilroy <jeffkilroy@gmail.com>
2017-10-24 15:25:12 -0400

    Update ZapMap.php

--- src/Facades/ZapMap.php
+++ src/Facades/ZapMap.php
@@ -3,9 +3,16 @@
 namespace ZapMap\ZapMapLaravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use ZapMap\ZapMapLaravel\Support\Testing\Fakes\ZapMapFake;
 
 class ZapMap extends Facade
 {
+
+    public static function fake()
+    {
+        static::swap(new ZapMapFake);
+    }
+
     protected static function getFacadeAccessor()
     {
         return 'zapmap';
